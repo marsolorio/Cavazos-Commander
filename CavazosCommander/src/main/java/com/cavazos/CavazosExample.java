1b271600f44d22d49bfb6bc4445e2cb979a94d2e
MARTIN SOLORIO <85971614+marsolorio@users.noreply.github.com>
2024-10-24 16:06:56 -0700

    Added text-based menu: Implemented initial menu layout

--- CavazosCommander/src/main/java/com/cavazos/CavazosExample.java
+++ CavazosCommander/src/main/java/com/cavazos/CavazosExample.java
@@ -1,56 +1,92 @@
 package com.cavazos;
 
+import java.io.FileReader;
+import java.util.List;
 import java.util.Random;
-import org.json.simple.*;
+import java.util.Scanner;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class CavazosExample {
-
   public static void main(String[] args) {
-    String fileName = "/Users/martinsolorio/Desktop/Cavazos-Commander/CavazosCommander/src/main/resources/commands.json";
+    Scanner scanner = new Scanner(System.in);
+    String input = "";
 
-    // read coammands
-    JSONArray commandJSONArray = JSONFile.readArray(fileName);
-    String[] commandArray = getCommandArray(commandJSONArray);
-    System.out.println(commandArray);
+    // Command loop
+    while (!input.equals("q")) {
+      // Display the menu
+      System.out.println("Cavazos Commander Menu:");
+      System.out.println("i - Issue a command");
+      System.out.println("l - List all commands");
+      System.out.println("u - Undo last command");
+      System.out.println("r - Redo last command");
+      System.out.println("q - Quit");
 
-    // print list of all commands
-    System.out.println("----- List of all commands -----");
-    print(commandArray);
+      // Get user input
+      input = scanner.nextLine();
 
-    System.out.println(
-        "----- Issuing 5 random commands from General Cavazos -----");
-    randomCommand(commandArray, 5);
+      // Handle user input
+      switch (input) {
+        case "i":
+          issueRandomCommand();
+          break;
+        case "l":
+          listAllCommands();
+          break;
+        case "u":
+          undoLastCommand();
+          break;
+        case "r":
+          redoLastCommand();
+          break;
+        case "q":
+          System.out.println("Goodbye!");
+          break;
+        default:
+          System.out.println("Invalid input. Please try again.");
+      }
+    }
+
+    scanner.close(); // Close the scanner to prevent resource leak
   }
 
-  // randomly issue commands from General Cavazos
-  public static void randomCommand(String[] commandArray, int numCommand) {
-    Random rand = new Random();
-    System.out.printf("Number\tCommand\n");
-    System.out.printf("------\t---------------\n");
-    for (int i = 0; i < numCommand; i++) {
-      int randIndex = rand.nextInt(commandArray.length);
-      System.out.printf("%04d\t%s\n", i, commandArray[randIndex]);
+  // Function to issue a random command from commands.json
+  private static void issueRandomCommand() {
+    try {
+      JSONParser parser = new JSONParser();
+      FileReader reader = new FileReader(CavazosExample.class.getClassLoader().getResource("commands.json").getFile());
+      JSONArray commands = (JSONArray) parser.parse(reader);
+      Random random = new Random();
+      int randomIndex = random.nextInt(commands.size());
+      System.out.println("Issued Command: " + commands.get(randomIndex));
+    } catch (Exception e) {
+      e.printStackTrace();
     }
   }
 
-  // print command array
-  public static void print(String[] commandArray) {
-    System.out.printf("Number\tCommand\n");
-    System.out.printf("------\t---------------\n");
-    for (int i = 0; i < commandArray.length; i++) {
-      System.out.printf("%04d\t%s\n", i, commandArray[i]);
+  // Function to list all commands from commands.json
+  private static void listAllCommands() {
+    try {
+      JSONParser parser = new JSONParser();
+      FileReader reader = new FileReader(CavazosExample.class.getClassLoader().getResource("commands.json").getFile());
+      JSONArray commands = (JSONArray) parser.parse(reader);
+      System.out.println("List of Commands:");
+      for (Object command : commands) {
+        System.out.println(command);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
   }
 
-  // get array of commands
-  public static String[] getCommandArray(JSONArray commandArray) {
-    String[] arr = new String[commandArray.size()];
+  // Placeholder for undo command
+  private static void undoLastCommand() {
+    System.out.println("Undo functionality not implemented yet.");
+  }
 
-    // get names from json object
-    for (int i = 0; i < commandArray.size(); i++) {
-      String command = commandArray.get(i).toString();
-      arr[i] = command;
-    }
-    return arr;
+  // Placeholder for redo command
+  private static void redoLastCommand() {
+    System.out.println("Redo functionality not implemented yet.");
   }
 }
